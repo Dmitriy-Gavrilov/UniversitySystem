510fe47d9fab8442f074a8e5796bd87546b2c8ae
Dmitriy <dmitriy100813@gmail.com>
2024-12-26 20:08:21 +0300

    add get_full_info into teacher route

--- src/teacher/services/service.py
+++ src/teacher/services/service.py
@@ -1,7 +1,9 @@
+from src.assignment.models import Assignment
 from src.core.database.repo import Repository
+from src.subject.models import Subject
 
 from src.teacher.models import Teacher
-from src.teacher.schemas import CreateTeacherSchema
+from src.teacher.schemas import CreateTeacherSchema, ResponseTeacherSchema
 from src.teacher.exceptions import TeacherNotFoundError
 
 
@@ -22,6 +24,21 @@ class TeacherService:
         if teacher:
             return teacher
         raise TeacherNotFoundError()
+
+    async def get_full_info(self, teacher_id: int, assignment_repo: Repository[Assignment],
+                            subject_repo: Repository[Subject]) -> ResponseTeacherSchema:
+        teacher = await self.repo.get(id=teacher_id)
+        if not teacher:
+            raise TeacherNotFoundError()
+
+        assignments = await assignment_repo.get_all(filters=[Assignment.teacher_id == teacher_id])
+
+        subjects = [(await subject_repo.get(id=a.subject_id)).subject_name for a in assignments]
+
+        teacher_data = {key: getattr(teacher, key) for key in teacher.__mapper__.c.keys()}
+        teacher_data["subjects"] = subjects
+
+        return ResponseTeacherSchema(**teacher_data)
 
     async def update(self, teacher_id: int, new_data: CreateTeacherSchema) -> Teacher:
         teacher = await self.repo.get(id=teacher_id)
