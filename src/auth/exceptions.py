1fc82f4ed642bf8f22bc2c40b69070860ef3e2ee
egorshv <egor.shvedov@buzz.ai>
2024-12-27 12:17:57 +0300

    fixes

--- src/auth/exceptions.py
+++ src/auth/exceptions.py
@@ -8,9 +8,9 @@ class OtherRoleRequired(BaseAppException):
 
 class AdminRoleRequired(BaseAppException):
     def __init__(self):
-        super().__init__(status_code=403, detail="У пользователя другая роль")
+        super().__init__(status_code=403, detail="Требуются права администратора")
 
 
 class TeacherRoleRequired(BaseAppException):
     def __init__(self):
-        super().__init__(status_code=403, detail="У пользователя другая роль")
+        super().__init__(status_code=403, detail="Требуются права учителя")
